c537e28323d9e6b14cc813a486a1d255e3736811
s.aran <s-aran@users.noreply.github.com>
2024-02-07 01:21:57 +0900

    implemented external editor settings

--- templay/src-tauri/src/main.rs
+++ templay/src-tauri/src/main.rs
@@ -7,10 +7,11 @@ pub mod external_editor;
 use std::fs::{self, remove_file, File};
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
+use std::sync::Mutex;
 
 use external_editor::ArgParams;
 use serde::{Deserialize, Serialize};
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+use tauri::{CustomMenuItem, Manager, Menu, MenuItem, Submenu};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -18,28 +19,51 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct ConfigTemplate {
     name: String,
     content: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct ConfigExternalEditor {
     name: String,
     command: String,
     args: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Config {
     version: u32,
     external_editor: Option<ConfigExternalEditor>,
     templates: Vec<ConfigTemplate>,
 }
 
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            version: 0,
+            external_editor: None,
+            templates: vec![],
+        }
+    }
+}
+
+struct ConfigState {
+    config: Mutex<Config>,
+}
+
+impl ConfigState {
+    fn new() -> Self {
+        let config = Config::default();
+        ConfigState {
+            config: Mutex::new(config),
+        }
+    }
+}
+
 #[tauri::command]
-fn load_config() -> Result<Config, String> {
+fn load_config(state: tauri::State<'_, ConfigState>) -> Result<Config, String> {
     let toml_content = fs::read_to_string("config.toml");
     if toml_content.is_err() {
         let message = format!("Error reading config file. {:?}", toml_content.err());
@@ -49,15 +73,19 @@ fn load_config() -> Result<Config, String> {
 
     let config: Config = toml::from_str(&toml_content.unwrap()).unwrap();
     println!("{:?}", config);
+
+    *state.config.lock().unwrap() = config.clone();
     Ok(config)
 }
 
 #[tauri::command]
-async fn save_config(config: Config) {
+fn save_config(state: tauri::State<'_, ConfigState>, config: Config) {
     let mut file = File::create("config.toml").unwrap();
     let toml_content = toml::to_string(&config).unwrap();
     write!(file, "{}", toml_content).unwrap();
     file.flush().unwrap();
+
+    *state.config.lock().unwrap() = config.clone();
 }
 
 fn initialize_tempfile(text: impl Into<String>) -> PathBuf {
@@ -80,17 +108,26 @@ fn initialize_tempfile(text: impl Into<String>) -> PathBuf {
 }
 
 #[tauri::command]
-fn open_by_external_editor(text: String) -> String {
+fn open_by_external_editor(state: tauri::State<'_, ConfigState>, text: String) -> String {
+    let config = state.config.lock().unwrap().clone();
+    let external_editor_config = match config.external_editor {
+        Some(e) => e,
+        None => {
+            eprintln!("external editor is not set.");
+            return text;
+        }
+    };
+
     // write
     let path = initialize_tempfile(&text);
 
     // edit
     let editor = external_editor::ExternalEditor::new(
-        "Vim",
+        external_editor_config.name,
         // "F:\\software\\vim\\vim90\\gvim.exe",
-        "notepad",
+        external_editor_config.command,
         // "F:\\software\\Notepad++\\notepad++.exe",
-        "{FilePath}",
+        external_editor_config.args,
     );
     let mut params = ArgParams::new();
     params.set_file_path(path.to_string_lossy());
@@ -142,6 +179,16 @@ fn main() {
                 event.window().close().unwrap();
             }
             _ => {}
+        })
+        .setup(|app| {
+            let state = ConfigState::new();
+            app.manage(state);
+
+            // 開発時は devtools を起動時に表示
+            #[cfg(debug_assertions)]
+            app.get_window("main").unwrap().open_devtools();
+
+            Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
