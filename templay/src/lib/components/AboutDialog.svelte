aeda600b5f2af5ecc26a44c6a2a00b466265f37b
s.aran <s-aran@users.noreply.github.com>
2024-02-23 16:39:10 +0900

    add about dialog

---
+++ templay/src/lib/components/AboutDialog.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import About from "../pages/About.svelte";
+
+  export let showDialog: boolean;
+
+  let dialog: HTMLDialogElement;
+  $: {
+    if (dialog && showDialog) dialog.showModal();
+  }
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<dialog
+  bind:this={dialog}
+  on:close={() => (showDialog = false)}
+  on:click|self={() => {
+    dialog.close();
+  }}
+>
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div>
+    <About></About>
+  </div>
+</dialog>
